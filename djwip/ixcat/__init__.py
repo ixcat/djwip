328abb07a2ec9f4f718856180906341b6dbe6d56
Chris Turner <cat@199tech.net>
2018-07-09 13:18:35 -0500

    djwip/ixcat/__init__.py: import dtutils to top

--- djwip/ixcat/__init__.py
+++ djwip/ixcat/__init__.py
@@ -4,5 +4,13 @@ from .csvex import CSVBase
 from .csvex import CSVColumnDict
 from .csvex import CSVRowDict
 
+from .dtutils import dt_slice
+from .dtutils import dt_trunc
+from .dtutils import dt_tostr
+from .dtutils import dt_fromstr
+from .dtutils import before
+from .dtutils import after
+from .dtutils import between
 
-__all__ = [CSVBase, CSVColumnDict, CSVRowDict]
+__all__ = [CSVBase, CSVColumnDict, CSVRowDict,
+           dt_slice, dt_trunc, dt_fromstr, dt_tostr, before, after, between]
